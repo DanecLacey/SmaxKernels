1960acddf6b22c340e248fa6011516e0b72af390
Siram Katta <sriram.katta@fau.de>
2025-05-08 17:19:37 +0200

    add header file cuda.hpp to include necessary CUDA utilities

---
+++ src/platforms/cuda/cuda.hpp
@@ -0,0 +1,10 @@
+#ifndef CUDA_HPP
+#define CUDA_HPP
+
+#include "cuda_error_handler.cuh"
+#include "cuda_helper.cuh"
+#include "cuda_timer.cuh"
+#include "event_helper.cuh"
+#include "stream_helper.cuh"
+
+#endif
