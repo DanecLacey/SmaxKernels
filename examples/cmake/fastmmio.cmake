374d5d74dac1a013ea573ce1ada94a2978ea0418
Siram Katta <sriram.katta@fau.de>
2025-05-30 16:27:20 +0200

    [ADD] fmmio package configuration to CMake

---
+++ examples/cmake/fastmmio.cmake
@@ -0,0 +1,8 @@
+CPMAddPackage(
+    NAME fmmio
+    GITHUB_REPOSITORY alugowski/fast_matrix_market
+    GIT_TAG v1.7.6
+)
+
+add_library(fmmio INTERFACE)
+target_include_directories(fmmio INTERFACE ${fmmio_SOURCE_DIR}/include)
