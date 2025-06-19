3829973ce7763e335aab58510679cbf8e08024fd
Sriram Katta <sriram.katta@fau.de>
2025-06-20 00:55:48 +0200

    refactor fastmmio.cmake for improved conditional structure

--- examples/cmake/fastmmio.cmake
+++ examples/cmake/fastmmio.cmake
@@ -1,8 +1,10 @@
 CPMAddPackage(
-    NAME fmmio
-    GITHUB_REPOSITORY alugowski/fast_matrix_market
-    GIT_TAG v1.7.6
+  NAME fmmio
+  GITHUB_REPOSITORY alugowski/fast_matrix_market
+  GIT_TAG v1.7.6
 )
 
-add_library(fmmio INTERFACE)
-target_include_directories(fmmio INTERFACE ${fmmio_SOURCE_DIR}/include)
+if(fmmio_ADDED)
+  add_library(fmmio INTERFACE)
+  target_include_directories(fmmio INTERFACE ${fmmio_SOURCE_DIR}/include)
+endif()
