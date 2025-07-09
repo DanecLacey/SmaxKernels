0919c522e49f799f206d2b4e663af1e07b532f23
Sriram Katta <sriram.katta@fau.com>
2025-07-09 19:07:33 +0200

    [ADD] create common header for cusparse benchmarks with error checking macros

---
+++ examples/benchmarks/cusparse_benchmarks/cusparse_benchmarks_common.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#define MKL_AGGRESSIVE_N_OPS 10000
+
+#include <cuda_runtime_api.h> 
+#include <cusparse.h>
+
+#define CHECK_CUDA(func)                                                       \
+do{                                                                              \
+    cudaError_t status = (func);                                               \
+    if (status != cudaSuccess) {                                               \
+        printf("CUDA API failed at line %d with error: %s (%d)\n",             \
+               __LINE__, cudaGetErrorString(status), status);                  \
+        return EXIT_FAILURE;                                                   \
+    }                                                                          \
+}while(0)
+
+#define CHECK_CUSPARSE(func)                                                   \
+do{                                                                              \
+    cusparseStatus_t status = (func);                                          \
+    if (status != CUSPARSE_STATUS_SUCCESS) {                                   \
+        printf("CUSPARSE API failed at line %d with error: %s (%d)\n",         \
+               __LINE__, cusparseGetErrorString(status), status);              \
+        return EXIT_FAILURE;                                                   \
+    }                                                                          \
+}while(0)
+
