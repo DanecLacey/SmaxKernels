c801f5db1e9a199ab58bf4fde4535aaf6396d202
Jannik Hausladen <jannik.hausladen@fau.de>
2025-07-17 17:54:14 +0200

    [FIX] NONE type permutation fixed

--- src/utils/permutations.hpp
+++ src/utils/permutations.hpp
@@ -521,11 +521,12 @@ void Utils::generate_perm(int A_n_rows, IT *A_row_ptr, IT *A_col, int *perm,
         n_levels = Utils::generate_color_perm_bal(A_n_rows, A_sym_row_ptr,
                                                   A_sym_col, lvl, n_levels);
     } else if (type == "NONE") {
-// Generates dummy permutation
+    // Generates dummy permutation
 #pragma omp parallel for
         for (int i = 0; i < A_n_rows; ++i) {
-            lvl[i] = 0;
+            lvl[i] = i;
         }
+        n_levels = A_n_rows;
     } else {
         // Throwing errors in lib is not nice.
         // TODO: think of a way to tell user that the wrong type is
