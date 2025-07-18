40c52e436325ef8e459782d7fceaba0fab71cbad
Sriram Katta <sriram.katta@fau.com>
2025-07-18 18:27:55 +0200

    GPU Independet Backend Init
    [REMOVE] vendor specific platform module
    
    [ADD] GPU backend support with device management, error handling, and stream/event management
    
    [FIX] Update CUDA header directory path from 'cuda' to 'gpu'
    
    [UPDATE] include paths from 'cuda' to 'gpu' for consistency across implementations
    
    [UPDATE] Replace CUDA event handling with SMAX GPU event management in BenchHarnessCUDA
    
    [FIX] Comment out synchronization calls in BenchHarnessCUDA to evade over synchronization
    
    [FIX] Specify template type in memcpy and asyncMemcpy functions for improved type safety
    
    [REMOVE] Delete gpu_attributes.hpp and gpu_device_manager.hpp files to streamline GPU platform code
    
    This is a combination of 5 commits.
    
    [UPDATE] Refactor error handling functions in gpu_error_handler.hpp for improved readability and consistency
    [ADD] GPU_SAFE_BACKEND_CALL
    
    [UPDATE] Replace error checking macros with GPU_SAFE_BACKEND_CALL for improved error handling consistency in gpu_stream and gpu_event classes
    
    [UPDATE] Replace error checking macros with GPU_SAFE_BACKEND_CALL for improved error handling in memory management functions
    
    [REMOVE] Remove inclusion of gpu_device_manager.hpp to streamline GPU manager dependencies
    
    [UPDATE] Comment out GPU_SAFE_BACKEND_CALL in transfer_DtoH free the device data

---
+++ src/platforms/gpu/gpu_manager.hpp
@@ -0,0 +1,15 @@
+#ifndef GPU_MANAGER_HPP
+#define GPU_MANAGER_HPP
+#pragma once
+
+#include "../../macros.hpp"
+#include "../../memory_utils.hpp"
+
+#include "gpu_backend.hpp"
+#include "gpu_error_handler.hpp"
+#include "gpu_flags.hpp"
+
+#include "gpu_stream_event_manager.hpp"
+#include "gpu_memory_manger.hpp"
+
+#endif
